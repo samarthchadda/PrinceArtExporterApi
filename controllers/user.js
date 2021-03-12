f71b28d62dc584210cc14503a915f2f669c48ccc
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-03-12 08:11:53 +0530

    userLoginStatus1

--- controllers/user.js
+++ controllers/user.js
@@ -54,7 +54,7 @@ exports.signInUser=(req,res,next)=>{
                 .then(user=>{
                     if(!user)
                     {
-                        return res.sendStatus(404).json({ message:'User does not exist',status:false});
+                        return res.status(404).json({ message:'User does not exist',status:false});
                     }
 
                     if(user.password == hex)
