00b14452adc20c9b40eecfed94a75fd9e37fa52e
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2021-03-18 19:34:47 +0530

    portIssue1

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/api',quotationRoutes);
 app.use('/api',productRoutes);
 
 
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 8080;
 //establishing DB connection
 mongoConnect(()=>{
      
